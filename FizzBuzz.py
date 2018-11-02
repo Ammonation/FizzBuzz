965fa120a4e52d296e833af645a86abaa642a993
Ammonation <harry_a99@hotmail.co.uk>
2018-11-02 12:30:14 +0000

    Syntax Change

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -5,12 +5,10 @@ def fizzBuzz(start, end):
     for i in range(start, end+1): #set up for loop
         #Self Note: the range function starts at 0 and itterates by 1 until it reaches end
         output = "" #create empty string
-        if(i % 3 == 0): # checks if number is divisable by 3
-            output += str("Fizz")
-        if(i% 5 == 0): # checks if number is divisable by 5
-            output += str("Buzz")
-        if(output == ""): # if no text in string, add number to the string
-            output = str(i)
+        if(i % 3 == 0):output += str("Fizz") # checks if number is divisable by 3    
+        if(i% 5 == 0): output += str("Buzz") # checks if number is divisable by 5   
+        if(output == ""): output = str(i) # if no text in string, add number to the string
+            
         print (output) # print string
 
 fizzBuzz(1,100)
